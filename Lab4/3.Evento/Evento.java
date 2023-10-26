dd387934d5d4cb5d2d598e2a1de243592c0c8d95
SaraBuzzi <120629582+SaraBuzzi@users.noreply.github.com>
2023-10-26 10:48:49 +0200

    Lab4

---
+++ Lab4/3.Evento/Evento.java
@@ -0,0 +1,155 @@
+
+
+import java.util.Date;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+
+public class Evento implements Cloneable{
+    //OVERVIEW: modella un Evento con data e nome. Astrazione immutabile
+
+    //attributi
+    Date data;
+    String nome;
+
+
+    //construttori
+    public Evento(Date data, String nome) throws IllegalArgumentException, NullPointerException{
+        //MODIFIES: this
+        //EFFECTS: inizializza un oggetto Evento
+        //          se data null lancia NullPointerException
+        //          se nome null lancia NullPointerExcpetion
+        //          se nome vuoto lanica IllegalArgumentException
+
+        if (data == null) {
+            throw new NullPointerException("data nulla");
+        }
+
+        if (nome == null) {
+            throw new NullPointerException("nome nullo");
+        }
+
+        if (nome.equals("")) {
+            throw new IllegalArgumentException("nome vuoto");
+        }
+
+        this.nome = nome;
+        this.data = (Date) data.clone();
+
+        assert repOk();
+    }
+
+    public boolean repOk() {
+        if (this.data == null) {
+            return false;
+        }
+
+        if (this.nome == null) {
+            return false;
+        }
+
+        if (this.nome.equals("")) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+
+    //metodi
+    public Evento copiaEvento(int n) throws IllegalArgumentException{
+        //EFFECTS: creare un nuovo evento con this.nome e this.data+n
+        //         se n<=0 lancia IllegalArgumentException
+
+        if (n<=0) {
+            throw new IllegalArgumentException("n <= 0");
+        }   
+
+        Evento nuovo = (Evento) this.clone();
+        nuovo.data = new Date(nuovo.data.getTime() + TimeUnit.DAYS.toMillis(n));
+
+        return nuovo;
+    }
+
+    @Override
+    public Object clone() {
+       Evento nuovo = null;
+        try {
+            nuovo = (Evento) super.clone(); 
+        } catch (CloneNotSupportedException e) {
+            nuovo = new Evento(data, nome);
+        }
+        nuovo.data = (Date) this.data.clone();
+        return nuovo;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Evento other = (Evento) obj;
+        if (data == null) {
+            if (other.data != null)
+                return false;
+        } else if (!data.equals(other.data))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+    public Date getData() {
+        return (Date)data.clone();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Inserisci data del primo evento");
+
+        String[] pieces1 = s.next().split("/");
+        Date data1 = new Date(
+        Integer.parseInt(pieces1[2])-1900, 
+        Integer.parseInt(pieces1[1]), 
+        Integer.parseInt(pieces1[0]));
+
+        System.out.println("Inserisci nome del primo evento");
+
+        String nome1 = s.next();
+
+        System.out.println("Inserisci data del secondo evento");
+
+        String[] pieces2 = s.next().split("/");
+        Date data2 = new Date(
+            Integer.parseInt(pieces2[2])-1900, 
+            Integer.parseInt(pieces2[1]), 
+            Integer.parseInt(pieces2[0]));
+        
+        System.out.println("Inserisci nome del secondo evento");
+
+        String nome2 = s.next();
+
+        Evento e1 = new Evento(data1, nome1);
+        Evento e2 = new Evento(data2, nome2);
+
+        if (e1.equals(e2)) {
+            System.out.println("I due eventi sono uguali");
+        } else {
+            System.out.println("I due eventi sono diversi");
+        }
+    }
+
+}
---
+++ Lab4/6.Tris/Tris.java
@@ -0,0 +1,143 @@
+import java.util.Scanner;
+
+public class Tris {
+    //OVERVIEW: classe Tris che modelli il gioco del tris con due giocatori
+
+    //attributi
+    char[][] tabellone;
+    char turno;
+    int numMosse;
+
+    //construttori
+    public Tris() {
+        //EFFECTS: crea un nuovo gioco col tabellone vuoto e assegna il turno al giocatore O
+        
+        this.tabellone = new char[3][3];
+        this.turno = 'O';
+    
+    }
+
+    //metodi
+    public char turno() {
+        //restituisce il carattere del giocatore di turno (X o O)
+        return this.turno;
+        
+    }
+
+    public void mossa(int x, int y) throws IllegalArgumentException {
+        //MODIFIES: this
+        //EFFECTS: inserisce il carattere del giocatore di turno alle coordinate specificate, tiene conto del numerio di mosse effettuate e cambia turno di gioco
+        //          se la posizione è occupata lancia IllegalArgumentException
+        //          se x o y non rientrano nel range [1-3] lancia IllegalArgumentException
+
+        if ( x < 1 || x > 3 || y < 1 || y > 3) {
+            throw new IllegalArgumentException("coordinate non valide");
+        }
+        if (tabellone[x-1][y-1] != 0) {
+            throw new IllegalArgumentException("posizione occupata");
+        }
+        
+        this.tabellone[x-1][y-1] = this.turno;
+        this.numMosse++;
+        cambiaTurno();
+    }
+
+    public void cambiaTurno() {
+        //MODIFIES: this
+        //EFFECTS: cambia turno a this
+
+        if (this.turno == 'O') {
+            this.turno = 'X';
+        } else {
+            this.turno = 'O';
+        }
+    }
+
+
+    public boolean terminato() {
+        //EFFECTS: restituisce true se sono state giocate tutte e 9 le mosse
+
+        return this.numMosse == 9;
+    }
+
+    public boolean vittoria() {
+        //EFFECTS: restituisce treu se il giocatore di turno ha vinto (tre dei suoi simboli in fila su riga, colonna o diagonale)
+        
+        if (this.tabellone[0][0] == this.tabellone[1][1] && this.tabellone[1][1] == this.tabellone[2][2]) {
+            if (this.tabellone[0][0] == this.turno) {
+                return true;
+            }
+            
+        }
+
+        if (this.tabellone[0][2] == this.tabellone[1][1] && this.tabellone[1][1] == this.tabellone[2][0]) {
+            if (this.tabellone[0][2] == this.turno) {
+                return true;
+            }
+            
+        }
+
+        int i = 0;
+        int j = 0;
+            if (this.tabellone[i][j] == this.tabellone[i][j+1] && this.tabellone[i][j] == this.tabellone[i][j+2]) {
+                if (this.tabellone[i][j] == this.turno) {
+                    return true;
+                }
+            
+            }
+
+            if (this.tabellone[j][i] == this.tabellone[j+1][i] && this.tabellone[j][i] == this.tabellone[j+2][i]) {
+                if (this.tabellone[0][0] == this.turno) {
+                    return true;
+                }
+            
+            }
+        return false;
+    }
+
+
+    @Override
+    public String toString() {
+        String ret = "-----------------\n";
+        ret += "|R\\C| 1 | 2 | 3 |\n";
+        ret += "-----------------\n";
+            
+        for (int i = 0; i < tabellone.length; i++) {
+                ret += "| " + (i+1) + " |";
+                for (int j = 0; j < tabellone.length; j++) {
+                    if (this.tabellone[i][j] == 0 ) {
+                        ret += "   |";
+                    } else {
+                      ret += " " + this.tabellone[i][j] + " |";     
+                    }
+                    
+                }
+                ret += "\n-----------------\n";
+            }
+
+            return ret;
+
+    }
+
+    public static void main(String[] args) {
+        Tris nuovo = new Tris();
+            
+        Scanner s = new Scanner(System.in);
+
+        while (!nuovo.terminato()) {
+            System.out.println("Mossa di " + nuovo.turno);
+            System.out.println("Inserisci: x y");
+            nuovo.mossa(s.nextInt(), s.nextInt());
+
+            System.out.println(nuovo);
+
+            if (nuovo.vittoria()) {
+                System.out.println("Ha vinto " + nuovo.turno);
+                System.exit(1);
+            }
+
+        } 
+        System.out.println("Partita terminata in pareggio");
+
+    }
+}
